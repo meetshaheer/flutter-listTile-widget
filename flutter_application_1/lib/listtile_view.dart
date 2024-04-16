e05942f15135d3d664b7909b96a223c860c2ac51
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-04-16 11:42:36 +0500

    Wrap Icon widget with padding widget for apply left padding to icons

--- flutter_application_1/lib/listtile_view.dart
+++ flutter_application_1/lib/listtile_view.dart
@@ -15,7 +15,95 @@ class listTile_view extends StatelessWidget {
               backgroundColor: Colors.deepPurple,
               radius: 50,
             ),
-            
+            title: Text("Salman Ahmed KHI"),
+            subtitle: Text("Bhai Kahan ho..."),
+            trailing: Column(
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Text("7:00 am"),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Icon(
+                    Icons.fiber_manual_record,
+                    color: Colors.purple,
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            tileColor: Color.fromARGB(255, 205, 249, 255),
+            leading: CircleAvatar(
+              backgroundColor: Colors.deepPurple,
+              radius: 50,
+            ),
+            title: Text("Salman Ahmed KHI"),
+            subtitle: Text("Bhai Kahan ho..."),
+            trailing: Column(
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Text("7:00 am"),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Icon(
+                    Icons.fiber_manual_record,
+                    color: Colors.purple,
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            tileColor: Color.fromARGB(255, 205, 249, 255),
+            leading: CircleAvatar(
+              backgroundColor: Colors.deepPurple,
+              radius: 50,
+            ),
+            title: Text("Salman Ahmed KHI"),
+            subtitle: Text("Bhai Kahan ho..."),
+            trailing: Column(
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Text("7:00 am"),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Icon(
+                    Icons.fiber_manual_record,
+                    color: Colors.purple,
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            tileColor: Color.fromARGB(255, 205, 249, 255),
+            leading: CircleAvatar(
+              backgroundColor: Colors.deepPurple,
+              radius: 50,
+            ),
+            title: Text("Salman Ahmed KHI"),
+            subtitle: Text("Bhai Kahan ho..."),
+            trailing: Column(
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Text("7:00 am"),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Icon(
+                    Icons.fiber_manual_record,
+                    color: Colors.purple,
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
