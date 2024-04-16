b9deaccd376763cfdf102c03a4f34aea5670100e
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-04-16 12:11:51 +0500

    Creating scrolled Basic Chat Screen with List Tile view, Circle Avator, and different Icons.

--- flutter_application_1/lib/listtile_view.dart
+++ flutter_application_1/lib/listtile_view.dart
@@ -7,105 +7,465 @@ class listTile_view extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
         body: SafeArea(
-      child: Column(
-        children: [
-          ListTile(
-            tileColor: Color.fromARGB(255, 205, 249, 255),
-            leading: CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              radius: 50,
-            ),
-            title: Text("Salman Ahmed KHI"),
-            subtitle: Text("Bhai Kahan ho..."),
-            trailing: Column(
-              children: [
-                SizedBox(
-                  height: 15,
-                ),
-                Text("7:00 am"),
-                Padding(
-                  padding: EdgeInsets.only(left: 30),
-                  child: Icon(
-                    Icons.fiber_manual_record,
-                    color: Colors.purple,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
                   ),
-                )
-              ],
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.push_pin,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          ListTile(
-            tileColor: Color.fromARGB(255, 205, 249, 255),
-            leading: CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              radius: 50,
-            ),
-            title: Text("Salman Ahmed KHI"),
-            subtitle: Text("Bhai Kahan ho..."),
-            trailing: Column(
-              children: [
-                SizedBox(
-                  height: 15,
-                ),
-                Text("7:00 am"),
-                Padding(
-                  padding: EdgeInsets.only(left: 30),
-                  child: Icon(
-                    Icons.fiber_manual_record,
-                    color: Colors.purple,
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
                   ),
-                )
-              ],
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.push_pin,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          ListTile(
-            tileColor: Color.fromARGB(255, 205, 249, 255),
-            leading: CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              radius: 50,
-            ),
-            title: Text("Salman Ahmed KHI"),
-            subtitle: Text("Bhai Kahan ho..."),
-            trailing: Column(
-              children: [
-                SizedBox(
-                  height: 15,
-                ),
-                Text("7:00 am"),
-                Padding(
-                  padding: EdgeInsets.only(left: 30),
-                  child: Icon(
-                    Icons.fiber_manual_record,
-                    color: Colors.purple,
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
                   ),
-                )
-              ],
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.push_pin,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          ListTile(
-            tileColor: Color.fromARGB(255, 205, 249, 255),
-            leading: CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              radius: 50,
-            ),
-            title: Text("Salman Ahmed KHI"),
-            subtitle: Text("Bhai Kahan ho..."),
-            trailing: Column(
-              children: [
-                SizedBox(
-                  height: 15,
-                ),
-                Text("7:00 am"),
-                Padding(
-                  padding: EdgeInsets.only(left: 30),
-                  child: Icon(
-                    Icons.fiber_manual_record,
-                    color: Colors.purple,
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                )
-              ],
+                  Text("7:00 am"),
+                ],
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            ListTile(
+              tileColor: Color.fromARGB(255, 205, 249, 255),
+              leading: CircleAvatar(
+                backgroundColor: Colors.deepPurple,
+                radius: 50,
+              ),
+              title: Text("Salman Ahmed KHI"),
+              subtitle: Text("Bhai Kahan ho..."),
+              trailing: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("7:00 am"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Icon(
+                      Icons.fiber_manual_record,
+                      color: Colors.purple,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
